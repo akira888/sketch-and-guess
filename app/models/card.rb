98e738af62a02ae991361a871075bdc10fd0df3a
akira888 <a.cup.of.happiness@gmail.com>
2026-01-03 00:38:17 +0900

    カードの寿命は1Hで十分だった

--- app/models/card.rb
+++ app/models/card.rb
@@ -10,6 +10,10 @@
 #   # Later...
 #   card = Card.find("some_id")
 #   card.destroy
+#
+# カードというデータ構造はなく、 Prompt#card_num によってお題が６つごとにグルーピングされているところから
+# card has_many prompts のような状態になるため擬似的なデータ構造をモデルで表現している
+# ユーザーはcard単位でお題候補を一時的に保持し、乱数ルーレットにより手持ちのカードから一つのお題が選定される
 class Card < CacheModel
   # Attributes
   attribute :user_id, :integer
@@ -25,7 +29,7 @@ class Card < CacheModel
   end
 
   def self.default_ttl
-    1.day
+    1.hour
   end
 
   # Custom methods
